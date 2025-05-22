8dafca17b099a0f16a45a7f54da8879324fef234
0xIkari <zachary@zachary-miller.com>
2025-05-22 08:00:24 -0400

    Holy shit we got 100% coverage

--- tests/test_depscan.py
+++ tests/test_depscan.py
@@ -85,3 +85,100 @@ def test_bad_syntax_graceful_fail():
     print_refs("BAD SYNTAX", results)
 
     assert results == []
+
+def test_import_without_alias():
+    code = "import requests"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    assert len(results) == 1
+    assert results[0].imported_symbols == []
+
+def test_relative_import_level_2():
+    code = "from .. import configtools"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    assert len(results) == 1
+    assert results[0].module == '..'
+    assert results[0].imported_symbols == ['configtools']
+
+
+def test_from_import_no_level():
+    code = "from flask import Flask"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    assert results[0].module == 'flask'
+
+def test_import_module_wrong_object():
+    code = "getlib().import_module('something')"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    print_refs("IMPORTLIB INVALID STRUCTURE", results)
+
+    assert all(r.module != 'something' for r in results)
+
+def test_import_from_without_module():
+    code = "from  import foo"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    print_refs("FROM IMPORT MISSING MODULE", results)
+
+    assert results == []
+
+def test_relative_import_with_module():
+    code = "from ..utils import foo"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    print_refs("RELATIVE IMPORT WITH MODULE", results)
+
+    assert len(results) == 1
+    assert results[0].module == '..utils'
+    assert results[0].imported_symbols == ['foo']
+
+def test_call_name_not_import():
+    code = "do_something('foo')"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    print_refs("CALL NAME NOT IMPORT", results)
+
+    assert all(r.import_type != 'dynamic' for r in results)
+
+def test_import_module_wrong_id():
+    code = """
+class Dummy:
+    id = 'not_importlib'
+Dummy().import_module('notreal')
+"""
+    tmp_path = write_temp_script(code)
+    results = scan_script_for_imports(tmp_path)
+    print_refs("IMPORT MODULE WRONG ID", results)
+    assert all(r.module != 'notreal' for r in results)
+
+def test_import_module_non_string_arg():
+    code = "import importlib\nimportlib.import_module(42)"
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    print_refs("IMPORTLIB NON-STRING ARG", results)
+
+    assert all(r.module != '42' for r in results)
+
+def test_dunder_import_variable_arg():
+    code = '''
+mod = "json"
+__import__(mod)
+'''
+    tmp_path = write_temp_script(code)
+
+    results = scan_script_for_imports(tmp_path)
+    print_refs("DUNDER IMPORT VAR ARG", results)
+
+    assert all(r.import_type != 'dynamic' for r in results)
+
+# Cucumbers
